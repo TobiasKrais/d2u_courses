1666528dc03ce4c61d5e1594064d3d120fb94ae1
Tobias Krais <tk@design-to-use.de>
2023-05-24 18:14:00 +0200

    prepare next version

--- pages/setup.php
+++ pages/setup.php
@@ -234,12 +234,16 @@ if ('d2u_courses' === rex_request('import', 'string') && $old_tables_available) 
 	<form action="https://www.paypal.com/cgi-bin/webscr" method="post" target="_top">
 		<input type="hidden" name="cmd" value="_s-xclick" />
 		<input type="hidden" name="hosted_button_id" value="CB7B6QTLM76N6" />
-		<input type="image" src="https://www.paypalobjects.com/de_DE/DE/i/btn/btn_donateCC_LG.gif" border="0" name="submit" title="PayPal - The safer, easier way to pay online!" alt="Spenden mit dem PayPal-Button" />
-		<img alt="" border="0" src="https://www.paypal.com/de_DE/i/scr/pixel.gif" width="1" height="1" />
+		<input type="image" src="https://www.paypalobjects.com/de_DE/DE/i/btn/btn_donateCC_LG.gif" name="submit" title="PayPal - The safer, easier way to pay online!" alt="Spenden mit dem PayPal-Button" />
+		<img alt="" src="https://www.paypal.com/de_DE/i/scr/pixel.gif" width="1" height="1" />
 	</form>
 </fieldset>
 
 <h2>Changelog</h2>
+<p>3.3.3-DEV:</p>
+<ul>
+	<li>...</li>
+</ul>
 <p>3.3.2:</p>
 <ul>
 	<li>Bugfix: Speichern von Kursen endete wegen fehlender Wertumwandlung in Fehler.</li>
