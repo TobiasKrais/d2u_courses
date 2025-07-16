6319e8ffe7dc619cae8647606dfffd21f49201af
Tobias Krais <tk@design-to-use.de>
2025-07-16 09:40:31 +0200

    prepare next version

--- pages/setup.php
+++ pages/setup.php
@@ -240,6 +240,10 @@ if ('d2u_courses' === rex_request('import', 'string') && $old_tables_available) 
 </fieldset>
 
 <h2>Changelog</h2>
+<p>3.5.2-DEV:</p>
+<ul>
+	<li>...</li>
+</ul>
 <p>3.5.1:</p>
 <ul>
 	<li>Bugfix: Wenn das Buchungsdaten Plugin aktiviert ist, werden die Anzahl der Teilnehmer und Warteliste berechnet. Diese Felder werden somit ausgeblendet.</li>
