698cf4182391ac17cf48622152af41859e1ac334
Tobias Krais <tk@design-to-use.de>
2026-08-09 11:18:03 +0200

    removed unused wrapper

--- pages/help.php
+++
@@ -1,6 +0,0 @@
-<?php
-$readmePath = rex_path::addon('d2u_courses', 'README.md');
-$readmeContent = rex_file::get($readmePath);
-if(null !== $readmeContent) {
-    echo rex_markdown::factory()->parse($readmeContent);
-}
