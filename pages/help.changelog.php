d7014b61c5f82e3d08f5f305fa735d323b228254
Tobias Krais <tk@design-to-use.de>
2026-08-09 11:20:19 +0200

    prepare next version

--- pages/help.changelog.php
+++ pages/help.changelog.php
@@ -1,4 +1,8 @@
 <h2>Changelog</h2>
+<p>3.6.3-DEV:</p>
+<ul>
+	<li>...</li>
+</ul>
 <p>3.6.2:</p>
 <ul>
 	<li>Wartung: Interne CI-Dateien im Verzeichnis <code>.github</code> (GitHub Actions, Dependabot) werden nicht mehr mit dem Installer-Paket ausgeliefert.</li>
