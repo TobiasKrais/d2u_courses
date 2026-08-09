7e8da1fc9539bf170517c3ccd8980c2d935cf3aa
Tobias Krais <tk@design-to-use.de>
2026-08-09 11:04:01 +0200

    move page to use same logic in all addons

--- pages/help.readme.php
+++ pages/help.readme.php
@@ -1,3 +1,6 @@
 <?php
-
-include rex_path::addon('d2u_courses', 'pages/help.php');
+$readmePath = rex_path::addon('d2u_courses', 'README.md');
+$readmeContent = rex_file::get($readmePath);
+if(null !== $readmeContent) {
+    echo rex_markdown::factory()->parse($readmeContent);
+}
